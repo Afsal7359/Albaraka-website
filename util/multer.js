64d4b0a39a9593fff526c83f40a10b7fe90ff71a
Afsal7359 <afsal4771@gmail.com>
2023-09-15 17:24:20 +0530

    fix admin panel bugs

--- util/multer.js
+++ util/multer.js
@@ -5,10 +5,7 @@ module.exports = multer({
     storage: multer.diskStorage({}),
     fileFilter: (req, file, cb) => {
         let ext = path.extname(file.originalname);
-        if (ext !== '.pdf' && ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
-            cb(new Error('Unsupported file type'), false);
-            return;
-        }
+       
         cb(null, true);
     },
 });
